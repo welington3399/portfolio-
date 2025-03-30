6dd44fa6c0c45cae28ac879408eac0bd621b6852
Welington <welington13579@gmail.com>
2025-03-29 22:42:05 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,68 @@
+let nightday = document.getElementById("night-day")
+let imgnightday = document.getElementById("imgNightDay")
+let main = document.querySelector("body")
+let but = document.querySelector("button")
+let foto = document.querySelector(".foto-principal")
+
+//Abreviar nome
+window.onresize = ()=>{
+
+    let tela = window.innerWidth
+    if (tela > 1250){
+        document.querySelector("#MyName").innerHTML = "Welington Ferreira de Souza"
+
+        
+    }else{
+        document.querySelector("#MyName").innerHTML = "Welington F. S."
+
+    }
+}
+
+
+
+//MODO CLARO E ESCURO
+nightday.onclick = () =>{
+    if(imgnightday.src === "http://127.0.0.1:5500/img/sun_7218604.png"){
+        imgnightday.src = "./img/moon_560897.png"
+        main.classList.add ("color-darkMode")
+        main.classList.remove ("color-lightMode")
+        // foto.src = "./img/novoEuLight.svg"
+    }else{
+        imgnightday.src = "./img/sun_7218604.png"
+        main.classList.add ("color-lightMode")
+        main.classList.remove ("color-darkMode")
+        // foto.src = "./img/novoEu2.svg"
+        
+    }
+    // <img id="imgNightDay" src="./img/moon_560897.png" alt="">
+}
+
+//criando novos projetos
+let grid = document.querySelector("#projetos") 
+let project = document.querySelector(".grid-project")
+var img2 = document.querySelector(".box-img-project")
+
+let adicionar = (img, titulo, descrição, link)=>{
+    project.innerHTML +=
+
+    `
+                   <div class="box-project">
+                        <img class="box-img-project" src="./img/${img}.png" alt="">
+                        <div class="box-text-project">
+                            <h4>${titulo}</h4>
+                        <p> ${descrição}</p>
+                        </div>
+                        <a class="bot-proj" href="${link}">Saiba mais</a>
+                    </div>
+        `
+    
+}
+
+adicionar("proj_churrascometro", "Churrascometro", "Churrascometro é uma ferramenta que irá auxiliar você a saber o que precisa para um churrasco, baseado na quantidade de pessoas e tempo de churrasco.")
+adicionar("portifolioIMG", "Portifolio", "Aqui é onde você vai conhecer um pouco sobre mim, minhas habilidades e alguns projetos, tanto praticos quanto reais.")
+adicionar("novaBrasiliaIMG", "Nova Brasília", "Landing Page real feita para uma empresa que presta atividades de terceirização de serviços condominais.")
+
+// adicionar(img2.style.background = "url(../img/churrascomentroIMG.png)" , "curriculo", "teste teste teste")
+
+
+
